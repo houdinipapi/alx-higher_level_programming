c1a48094d9157b80e8f327286d24b20b22799a09
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-04 20:14:22 +0000

    Update

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 101):
-        if i % 3 == 0 and i % 5 == 0:
-           print("FizzBuzz ", end='')
-        elif i % 3 == 0:
-           print("Fizz ", end='')
-        elif i % 5 == 0:
-           print("Buzz ", end='')
+    for var in range(1, 101):
+        if var % 3 == 0 and var % 5 == 0:
+            print("FizzBuzz", end=' ')
+        elif var % 3 == 0:
+            print("Fizz", end=' ')
+        elif var % 5 == 0:
+            print("Buzz", end=' ')
         else:
-           print(i, end='')
-
-print(" ", end='')
+            print(f"{var:d}", end=' ')
