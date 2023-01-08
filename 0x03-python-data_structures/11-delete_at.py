564c67fb44d8259bf096d2e18da132cda11eaebc
houdinipapi <ochiengcliff.co@gmail.com>
2023-01-08 10:09:06 +0000

    delete update

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx0):
-    length = len(my_list)
 
-    if idx < 0 or idx >= length:
+    if idx < 0 or idx > len(my_list) - 1:
         return (my_list)
 
-    del my_list[idx]
+    else:
+        del my_list[idx]
 
     return (my_list)
