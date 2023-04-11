7e1a7e8d104890703f58f519219a7c351b4a50b8
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-11 20:11:09 +0000

    a script that computes and prints a factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
 function factorial (n) {
-  if (isNaN(n)) {
+  if (isNaN(n) || n === 0) {
     return 1;
   }
-  if (n <= 1) {
-    return 1;
+  if (n < 0) {
+    return -1;
   }
   return n * factorial(n - 1);
 }
 
 const num = parseInt(process.argv[2]);
 
-console.log(`Factorial of ${num}: ${factorial(num)}`);
+console.log(`${factorial(num)}`);
