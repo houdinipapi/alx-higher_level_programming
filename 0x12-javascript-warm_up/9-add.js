7149dc28bffc9fa44e164a189b9ff12277bd7895
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-11 10:51:35 +0000

    a script that prints the addition of 2 integers

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -8,7 +8,7 @@ const num1 = parseInt(process.argv[2]);
 const num2 = parseInt(process.argv[3]);
 
 if (isNaN(num1) || isNaN(num2)) {
-  console.log('Invalid input');
+  console.log('NaN');
 } else {
   console.log(add(num1, num2));
 }
