1d5d86d045c22c23de3d6ae949aecc7332c3d78e
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-12 16:54:47 +0000

    Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+
+// import the Square class
+const ParentSquare = require('./5-square');
+
+// define the Square class that inherits from Square
+class Square extends ParentSquare {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    // if c is undefined, use X as default
+    if (c === undefined) {
+      c = 'X';
+    }
+
+    // print the square using the character c
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+// export the Square class
+module.exports = Square;
