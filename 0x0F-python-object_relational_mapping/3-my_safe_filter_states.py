ead91d9ba37559c73f0f4c13e1e53e0df9409424
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-16 20:00:20 +0000

    This script queries the 'states' table in a MySQL database for rows with a given state name.

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 """
-    Aim: to write a script that is free from SQL injections
+This script queries the 'states' table in a MySQL database
+for rows with a given state name.
 """
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
+    # Connect to MySQL database using command line arguments
     conn = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+
+    # Create cursor object
     cur = conn.cursor()
+
+    # Execute SQL query with parameterized state name
+    # to avoid SQL injection attacks
     cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
                 (argv[4],))
+
+    # Fetch and display query results
     [print(state) for state in cur.fetchall()]
