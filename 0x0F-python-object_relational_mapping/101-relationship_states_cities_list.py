a7c41b20e1e438b0a81a714253f08e217ee2c893
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-16 23:07:50 +0000

    101-relationship_states_cities_list.py

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -9,12 +9,19 @@ from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
+    # Create a SQLAlchemy engine to connect to the MySQL server
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+
+    # Create a session factory to manage database connections
     Session = sessionmaker(bind=engine)
+
+    # Create a new session object
     session = Session()
 
+    # Query all State objects from the database and,
+    # print them along with their Cities
     for state in session.query(State).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
         for city in state.cities:
