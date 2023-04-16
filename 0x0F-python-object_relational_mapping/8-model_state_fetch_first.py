1309719bade63f36a0ee200393368ca0b9898f07
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-16 21:36:48 +0000

    This script prints the first State object from the database hbtn_0e_6_usa

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" prints the first State object from the database hbtn_0e_6_usa """
+"""
+This script prints the first State object from the database hbtn_0e_6_usa
+"""
 
 from sys import argv
 from sqlalchemy import create_engine
@@ -7,12 +9,19 @@ from model_state import State
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
+    # Connect to MySQL database using provided credentials
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
 
+    # Create a session maker object to handle database connections
     session_maker = sessionmaker(bind=engine)
+
+    # Create a new session object
     session = session_maker()
 
+    # Query the State table for the first State object
     state = session.query(State).order_by(State.id).first()
+
+    # Print the State object's id and name, or "Nothing" if no State is found
     print("Nothing" if state is None else f"{state.id}: {state.name}")
