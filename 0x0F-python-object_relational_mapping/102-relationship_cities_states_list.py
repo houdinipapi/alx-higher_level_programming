afd6ce0cebb1bc1c47f4ac3fea7286e653ac4476
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-16 23:12:09 +0000

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -9,11 +9,18 @@ from relationship_state import State
 from relationship_city import City
 
 if __name__ == "__main__":
+    # Create a new database engine using the provided command-line arguments
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
+
+    # Create a new session factory bound to the engine
     Session = sessionmaker(bind=engine)
+
+    # Create a new session object
     session = Session()
 
+    # Query all City objects from the database and order them by ID
     for city in session.query(City).order_by(City.id):
+        # Print the City ID, name, and the name of its associated State object
         print("{}: {} -> {}".format(city.id, city.name, city.state.name))
