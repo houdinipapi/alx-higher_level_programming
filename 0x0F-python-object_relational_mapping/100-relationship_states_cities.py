efe8414561f783fc5a8af067689c9e0d6b0c4a6f
houdinipapi <ochiengcliff.co@gmail.com>
2023-04-16 23:03:58 +0000

    100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -10,12 +10,21 @@ from relationship_state import State
 from relationship_city import Base, City
 
 if __name__ == "__main__":
+    # Create a connection to the MySQL database
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
+
+    # Create the tables defined in the models01~
     Base.metadata.create_all(engine)
+
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Create a new State and City object and associate them
+    # Add the City object to the session
     session.add(City(name="San Francisco", state=State(name="California")))
+
+    # Commit the changes
     session.commit()
