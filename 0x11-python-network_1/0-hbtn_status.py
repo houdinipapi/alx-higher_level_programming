1ae21e1ac8583c980e75371dd51bb39fb65f8b53
Omwami Ochieng <83884212+houdinipapi@users.noreply.github.com>
2023-05-02 00:35:55 +0300

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """Python script that fetches https://alx-intranet.hbtn.io/status"""
-import urllib.request
 
 if __name__ == "__main__":
+    from urllib.request import urlopen, Request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    url = "https://alx-intranet.hbtn.io/status"
+    req = Request(url)
+    with urlopen(req) as res:
         content = res.read()
 
-        print("Body response:")
-        print("\t- type:", type(content))
-        print("\t- contnet:", content)
-        print("\t- utf8 content:", content.decode('utf-8'))
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
