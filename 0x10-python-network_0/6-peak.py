5e08a8eac1b2ab829083d1881b7cad7e0f0ea637
houdinipapi <ochiengcliff.co@gmail.com>
2023-05-01 17:57:59 +0000

    Find a Peak

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""
-Finds a peak in a list of unsorted integers
-"""
+"""Sorting Algorithms in an integers' lists"""
 
 
 def find_peak(list_of_integers):
+    """Checks for the peak item in a list"""
     if list_of_integers:
         list_of_integers.sort(reverse=True)
         return list_of_integers[0]
