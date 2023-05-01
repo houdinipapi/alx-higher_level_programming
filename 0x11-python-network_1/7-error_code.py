3be66d4564fc194f14d27cb9a7e50591df2c50eb
houdinipapi <ochiengcliff.co@gmail.com>
2023-05-01 22:51:20 +0000

    Python script that takes in a URL, sends a request to the URL and displays the body of the response.

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-""" returns request content or error code with requests package """
+"""
+Python script that:
+- takes in a URL, sends a request to the URL
+- and displays the body of the response.
+"""
 from sys import argv
 from requests import get
 
