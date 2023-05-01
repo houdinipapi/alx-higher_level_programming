9f8556318114116a765af5a944651378d3b4e61f
houdinipapi <ochiengcliff.co@gmail.com>
2023-05-01 18:16:41 +0000

    Catch me if you can!

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request and the response is You got me!
-curl -s -X PUT -H "Origin: HolbertonSchool" -L 0.0.0.0:5000/catch_me --data ""
+curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
