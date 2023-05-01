6923b08f3344b7642177e7f9c89e3ac10f846d4e
Omwami Ochieng <83884212+houdinipapi@users.noreply.github.com>
2023-05-02 01:09:56 +0300

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,13 +5,13 @@ Python script that:
 - displays the body of the response (decoded in utf-8).
 """
 
+from sys import argv
+from urllib.request import urlopen
+from urllib.error import HTTPError
 
-if __name__ == "__main__":
-    import sys
-    from urllib import request, error
-
+if __name__ == '__main__':
     try:
-        with request.urlopen(sys.argv[1]) as res:
-            print(res.read().decode('UTF-8'))
-    except error.HTTPError as er:
-        print('Error code: {er.code}')
+        with urlopen(argv[1]) as res:
+            print(res.read().decode('utf-8'))
+    except HTTPError as err:
+        print(f'Error code: {err.code}')
