13f1d77c95bbb201b3b92c72ce363592cd09b5f1
houdinipapi <ochiengcliff.co@gmail.com>
2023-05-01 22:14:27 +0000

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-""" script that fetches https://alx-intranet.hbtn.io/status """
+"""
+Python script that:
+- fetches https://alx-intranet.hbtn.io/status
+"""
 from requests import get
 
 if __name__ == '__main__':
     res = get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
-    print('\t- type:', type(res.text))
-    print('\t- content:', res.text)
+    print(f'\t- type: {type(res.text)}')
+    print(f'\t- content: {res.text}')
