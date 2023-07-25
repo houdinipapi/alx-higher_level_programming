467b9760cfe9ce7e6c807f44be5b93fa82e9b4e4
papihoudini <ochiengcliff.co@gmail.com>
2023-07-25 18:09:49 -0400

    a script that computes the number of tasks completed by user id.

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,17 +1,26 @@
 #!/usr/bin/node
 
 const request = require('request');
-const endpoint = process.argv[2];
-const obj = {};
+const url = process.argv[2];
 
-request(endpoint, (err, res, body) => {
-  if (err) throw err;
-
-  JSON.parse(body).forEach(todo => {
-    const id = todo.userId;
-    if (!obj[id]) obj[id] = 0;
-    if (todo.completed) obj[id]++;
-    if (obj[id] === 0) delete obj[id];
-  });
-  console.log(obj);
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
 });
